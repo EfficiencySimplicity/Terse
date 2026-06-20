ca65900ccc4ca35e10398ab4aecddf4dca6a9d11
EfficiencySimplicity <joshua.ward@fastmail.com>
2026-06-19 20:24:11 -0400

    Added a comment about the CLI's functionality

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ fn main() {
     // a different module, and this logic must be somewhere,
     // Why not in main.rs???
     match cli {
+        // Because empty functions need empty files to accommodate them!
+        // stick it in CLI!
         Cli::Query(query) => process_query(query.words),
         _ => println!("AAAA")
         //Cli::Commands(commands_cli) => commands_cli.process(),
